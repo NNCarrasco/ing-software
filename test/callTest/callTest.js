010965153d0c899af2c303c344aee2867b7d2907
Nahuel Carrasco <nahuel.carrasco@cloudgaia.com>
2020-05-30 18:32:57 -0300

    edit

--- test/callTest/callTest.js
+++ test/callTest/callTest.js
@@ -1,16 +1,7 @@
 const init = () => {
-    let weather = "https://ws1.smn.gob.ar/v1/forecast/location/4852"; //conseguir los datos del tiempo de san miguel
     let alerts = "https://ws.smn.gob.ar/alerts/type/AL" //alertas
-	let token =
-		"eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ3ZWIiLCJzY29wZXMiOiJST0xFX1VTRVJfRk9SRUNBU1QsUk9MRV9VU0VSX0dFT1JFRixST0xFX1VTRVJfSElTVE9SWSxST0xFX1VTRVJfSU1BR0VTLFJPTEVfVVNFUl9NQVAsUk9MRV9VU0VSX1NUQVRJU1RJQ1MsUk9MRV9VU0VSX1dBUk5JTkcsUk9MRV9VU0VSX1dFQVRIRVIiLCJpYXQiOjE1OTA2MDQ4OTIsImV4cCI6MTU5MDY5MTI5Mn0.UpgCiiD3XqgotNcG3IElqGfOkza8xQO7HsAm19j28FE";
-	var weatherHeader = {
-		headers: {
-			Accept: "application/json",
-			Authorization: "JWT " + token,
-		},
-	};
-	fetch(weather, weatherHeader)
-	// fetch(alerts)
+	
+	fetch(alerts)
 		.then( response => response.json() )
 		.then( response => {
             console.log(response);
