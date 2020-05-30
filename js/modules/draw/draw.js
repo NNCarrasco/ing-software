01d84a7e84cf62458097ddb44c57fc86db54ce08
Nahuel Carrasco <nahuel.carrasco@cloudgaia.com>
2020-05-30 17:13:01 -0300

    create draw component

---
+++ js/modules/draw/draw.js
@@ -0,0 +1,38 @@
+import constructEmptyMessage from '../component/emptyMessage.js';
+import constructAdviceCard from '../component/adviceCard.js';
+import constructFilter from '../component/filter.js';
+import constructBussinesCard from '../component/businessCard.js';
+
+function construct (type, jQueryKey, elements, event = undefined){
+    let constructedList = [];
+    for(let element of elements){
+        constructedList.push(constructBy(type, element, event));
+    }
+    if (constructedList.length > 0) {
+		$(jQueryKey).append(constructedList);
+	} else {
+		constructEmptyMessage(jQueryKey);
+	}
+}
+
+function constructBy (type, element, event) {
+    let constructedElement;
+    switch (type) {
+        case "advice-card":
+            constructedElement = constructAdviceCard(element);
+            break;
+        case "filter":
+            constructedElement = constructFilter(element, event.onchange);
+            break;
+        case "business-card":
+            constructedElement = constructBussinesCard(element);
+            break;
+    }
+    return constructedElement;
+}
+
+function emptyElementChildOf(key) {
+    $(key).empty();
+}
+
+export {construct, emptyElementChildOf};
