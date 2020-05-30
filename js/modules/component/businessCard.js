f911d45a2f14e48b42ae28cabac40b7253a3ae50
Nahuel Carrasco <nahuel.carrasco@cloudgaia.com>
2020-05-30 17:10:38 -0300

    add business card

---
+++ js/modules/component/businessCard.js
@@ -0,0 +1,33 @@
+function constructBussinesCard(business) { //TODO: LAS VARIABLES ESTAN ES ESPAÑOL
+	return `
+    <div class="business-card" ${business.id? "id='" + business.id + "'" : ""} > 
+        <div class="header">
+            ${ business.nombre ? 
+                `<div class="name">
+                    ${ business.nombre }
+                </div>` : ""
+            }
+            ${ business.rubro ? 
+                `<div class="rubro">
+                    ${ business.rubro }
+                </div>` : ""
+            }
+            ${ business.telefono ? 
+                `<div class="phone">
+                    <i class="fas fa-phone-square phone-icon"></i>
+                    ${ business.telefono }
+                </div>` : ""
+            }
+        </div>
+        <div class="description">
+            ${ business.description ? 
+                `<span>
+                    ${ business.description }
+                </span>` : ""
+            }
+        </div>
+    </div>
+    `;
+}
+
+export default constructBussinesCard;
