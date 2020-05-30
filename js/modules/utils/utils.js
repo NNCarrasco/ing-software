2d15afa1030d99fdcfbe626ba11bf79db63cfa7b
Nahuel Carrasco <nahuel.carrasco@cloudgaia.com>
2020-05-30 17:15:21 -0300

    create utils component

---
+++ js/modules/utils/utils.js
@@ -0,0 +1,20 @@
+function getIcons(iconType, value){
+    if (iconType === "animal"){
+        switch (value) {
+            case 'Gato':
+                return '<i class="fas fa-cat"></i>';
+            case 'Perro':
+              return '<i class="fas fa-dog"></i>';
+          }
+    } else if (iconType === "gender") {
+        switch (value) {
+            case 'Macho':
+                return '<i class="fas fa-mars"></i>';
+            case 'Hembra':
+              return '<i class="fas fa-venus"></i>';
+          }
+    }
+    return '';
+}
+
+export default getIcons;
