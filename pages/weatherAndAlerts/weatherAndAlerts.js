5b7600c4c7458f52051edfeac7d6e46459200673
Ganzalo Moreira <gamoreira@proveedor.teco.com.ar>
2020-06-05 16:26:08 -0300

    forecats y today

--- pages/weatherAndAlerts/weatherAndAlerts.js
+++ pages/weatherAndAlerts/weatherAndAlerts.js
@@ -5,39 +5,21 @@ var alertKey = "#alert-list";
 var smn_alerts = "https://ws.smn.gob.ar/alerts/type/AL" //alertas SMN
 var heroku_alerts = "https://weatherservices.herokuapp.com/api/alerts/ByDay/0" //alertas SMN
 var heroku_weather = "https://weatherservices.herokuapp.com/api/weather/" //alertas SMN
-var weatherState;
+var todayWeather;
+var forecastWeather;
+
 const init = () => {
     fetch(heroku_weather)
     .then( response => response.json() )
     .then( response => {
-        weather = getWeather(response);
-        console.log(response);
+        todayWeather = getToday(response.items[0]);
+        forecastWeather = getForecast(Object.values(response.items[0].forecast.forecast));
+        console.log(response.items[0]);
     })
     .catch((error) => {
         console.log(error.message);
     });
-};
 
-function getWeather(weather){
-    let listForpronostic;
-    listForAlerts = alertList.map(a => {
-        return {date:a.date, hour:a.hour, status:a.status, title:a.title, description:a.description, zones:a.zones};
-    });
-    return listForAlerts;
-}
-function heroku_req(){
-    fetch(heroku_alerts)
-    .then( response => response.json() )
-    .then( response => {
-         alertList = getAlert(response.alerts);
-         construct('alert', alertKey, alertList);
-    })
-    .catch((error) => {
-        console.log(error.message);
-    });
-}
-
-function initAlerts(){
     fetch(smn_alerts)
 		.then( response => response.json() )
 		.then( response => {
@@ -53,6 +35,43 @@ function initAlerts(){
             console.log(error.message);
             heroku_req();
         });
+};
+
+function getToday(weather){
+    return { 
+        temp:weather.weather.temp, 
+        tempDesc:weather.weather.tempDesc,
+        humidity:weather.weather.humidity,
+        pressure:weather.weather.pressure,
+        description:weather.weather.description
+    };
+}
+
+function getForecast(forecast){
+    let listForForecast;
+    listForForecast = forecast.map(a => {
+        return {
+            date:a.date,
+            tempMin:a.temp_min, 
+            tempMax:a.temp_max, 
+            tempNoc:a.temp_noc, 
+            weatherMorning:a.morning.weather_id, 
+            weatherAfternoon:a.afternoon.weather_id};
+    });
+    return listForForecast;
+}
+
+
+function heroku_req(){
+    fetch(heroku_alerts)
+    .then( response => response.json() )
+    .then( response => {
+         alertList = getAlert(response.alerts);
+         construct('alert', alertKey, alertList);
+    })
+    .catch((error) => {
+        console.log(error.message);
+    });
 }
 
 function getAlert(alertList){
